2f97ae166bf4f8e8c060046e6dfaae183a59ef34
zeba1990 <46449272+zeba1990@users.noreply.github.com>
2019-07-11 16:56:11 +0200

    Add files via upload

---
+++ PositiveNum.java
@@ -0,0 +1,38 @@
+﻿
+/* 
+Positive number
+
+*/
+
+import java.io.*;
+import java.util.Scanner;
+
+public class PositiveNum {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+        Scanner obj = new Scanner(System.in);
+        int a = obj.nextInt();
+        Scanner obj1 = new Scanner(System.in);
+        int b = obj1.nextInt();
+        Scanner obj2 = new Scanner(System.in);
+        int c = obj2.nextInt();
+        int one = 1, two =2, three = 3, zero = 0;
+        
+        if (a > 0 && b <= 0 && c <= 0)
+        System.out.println(one);
+        else if (b > 0 && a <= 0 && c <=0)
+        System.out.println(one);
+        else if (c > 0 && a <= 0 && b <= 0)
+        System.out.println(one);
+        else if (a > 0 && b > 0 && c <= 0)
+        System.out.println(two);
+        else if (a > 0 && c > 0 && b <= 0)
+        System.out.println(two);
+        else if (c > 0 && b > 0 && a <=0)
+        System.out.println(two);
+        else if (a > 0 && b > 0 && c > 0)
+        System.out.println(three);
+        else if (a <= 0 && b <= 0 && c <= 0)
+        System.out.println(zero);
+    }   
+}
---
+++ PositiveNumReadme.txt
@@ -0,0 +1,6 @@
+﻿Use the keyboard to enter three integers. Display the number of positive numbers in the original set.
+The program should read the numbers from the keyboard.
+The program must display a number on the screen.
+The program should display the number of positive numbers in the original set.
+If there are no positive numbers, the program should display "0".
+Note that 0 is not a positive or negative number.
