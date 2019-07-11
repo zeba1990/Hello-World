1de5737b2776b6bc4461a67ed0f67e1238cbf9c4
zeba1990 <46449272+zeba1990@users.noreply.github.com>
2019-07-11 16:07:52 +0200

    Add files via upload

---
+++ DescribeNum.java
@@ -0,0 +1,42 @@
+﻿/* 
+Describing numbers
+
+*/
+
+import java.io.*;
+import java.util.Scanner;
+
+public class DescribeNum {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt();
+        int count=0;
+        int num = n;
+        while (n != 0) {
+            n = n/10;
+            count++;
+        }
+        
+        if (num >= 1 && num <=999){
+            if(num%2 == 0){
+                if(count == 1) {
+                    System.out.println("even single-digit number");
+                } else if (count == 2) {
+                    System.out.println("even two-digit number"); 
+                } else {
+                    System.out.println("even three-digit number");
+                }
+            }else {
+                if (count == 1) {
+                    System.out.println("odd single-digit number");
+                } else if (count == 2) {
+                    System.out.println("odd two-digit number"); 
+                } else {
+                    System.out.println("odd three-digit number" );
+                }
+        }
+        }
+
+    }
+}
---
+++ DescribeNumReadme.txt
@@ -0,0 +1,8 @@
+﻿Enter an integer from the keyboard in the range 1 - 999. Display a string description as follows:
+"even single-digit number" - if the number is even and has one digit,
+"odd single-digit number" - if the number is odd and has one digit,
+"even two-digit number" - if the number is even and has two digits,
+"odd two-digit number" - if the number is odd and has two digits,
+"even three-digit number" - if the number is even and has three digits,
+"odd three-digit number" - if the number is odd and has three digits.
+If the entered number does not fall in the range 1 - 999, don't display anything.
