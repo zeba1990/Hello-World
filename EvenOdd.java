185c68b0b0244ad8d789cf4ffaa844bf39a7ff9a
zeba1990 <46449272+zeba1990@users.noreply.github.com>
2019-07-11 14:43:32 +0200

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,28 @@
+﻿/* 
+Labels and numbers
+
+*/
+
+import java.io.*;
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt();
+        
+        if((n < 0) && (n%2 == 0)) {
+            System.out.println("Negative even number");
+        } else if ((n < 0)) {
+            System.out.println("Negative odd number");
+        } else if((n > 0) && (n%2 == 0)) {
+            System.out.println("Positive even number" );
+        } else if((n>0)  ) {
+             System.out.println("Positive odd number"); 
+        } else {
+            System.out.println("Zero");
+        }
+        
+    }
+}
---
+++ EvenOddReadme.txt
@@ -0,0 +1,6 @@
+﻿Use the keyboard to enter an integer. Display a string description as follows:
+"Negative even number" - if the number is negative and even,
+"Negative odd number" - if the number is negative and odd,
+"Zero" - if the number is 0,
+"Positive even number" - if the number is positive and even,
+"Positive odd number" - if the number is positive and odd.
