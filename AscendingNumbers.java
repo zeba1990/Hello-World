d214b482509d53f2c6a6b1100940468ef06e9185
zeba1990 <46449272+zeba1990@users.noreply.github.com>
2019-07-24 01:47:25 +0200

    Add files via upload

---
+++ AscendingNumbers.java
@@ -0,0 +1,26 @@
+﻿/* 
+Ascending numbers
+ Write a program that reads 5 numbers from the keyboard and displays them in ascending order.
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+
+
+public class AscendingNumbers {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int num[]=new int[5]; 
+        for(int i =0; i<5; i++){
+            String s = reader.readLine();
+            num[i] = Integer.parseInt(s);
+        }
+        Arrays.sort(num);
+        for(int i =0; i<5; i++){
+            System.out.println(num[i]);
+        }
+        //write your code here
+    }
+}
