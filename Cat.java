c120f6edafd23ac27c4cb53d52fafe66388ec240
zeba1990 <46449272+zeba1990@users.noreply.github.com>
2019-07-24 00:40:28 +0200

    Add files via upload

---
+++ Cat.java
@@ -0,0 +1,32 @@
+﻿/* 
+Static cats
+Practice with arraylist
+*/
+import java.util.ArrayList;
+
+
+
+public class Cat {
+    //write your code here
+    public static ArrayList<Cat> cats= new ArrayList<>(10);
+ 
+    public Cat() {
+        
+    }
+
+    public static void main(String[] args) {
+        //write your code here
+        printCats();
+        for(int i=0; i<10; i++){
+            cats.add(new Cat());
+        }
+    }
+
+    public static void printCats() {
+        //write your code here
+        for (int i = 0; i < 10; i++) {
+             System.out.println(cats);
+        }
+       
+    }
+}
